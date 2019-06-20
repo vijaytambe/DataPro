8d8f1ef99bb0f850311bd0438baf944d45a8af59
vijaytambe <vijay.tambe@questionpro.com>
2019-06-20 14:44:31 +0530

    First Commit

---
+++ src/main/java/com/questionpro/bi/QPDataPro/QpDataProApplication.java
@@ -0,0 +1,13 @@
+package com.questionpro.bi.QPDataPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpDataProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpDataProApplication.class, args);
+	}
+
+}
